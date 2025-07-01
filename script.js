01e01e5efe86086d8f4ab50e507d98ee4bd89545
joaoxxl <joao.arioso.costa@escola.pr.gov.br>
2025-07-01 11:26:45 -0300

    att2

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:Assim que saiu da escola hdhdhhdhdhddhdhdh
+        alternativas: [
+            "Isso é assustador!"
+            "Isso é incrível"
+        ]
+    }
+]
